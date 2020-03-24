6e5f04d781034a6344ba940f5629e1a1f9336a2b
AnanyaBottu <58173885+AnanyaBottu@users.noreply.github.com>
2020-03-24 18:21:25 +0530

    Add files via upload

---
+++ AngryBirdsStage2.5StudentActivity/Chain.js
@@ -0,0 +1,36 @@
+class Chain{
+    constructor(A,B){
+        var options = {
+            bodyA:A,stiffness:0.05,
+            bodyB:B,length:10
+        }
+         this.cons = Constraint.create(options);
+        World.add(world,this.cons);
+        
+
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    display(){
+        line (this.cons.bodyA.position.x,this.cons.bodyA.position.y,this.cons.bodyB.position.x,this.cons.bodyB.position.y);
+
+
+    }
+    
+
+
+
+
+
+
+
+
+
+} 
